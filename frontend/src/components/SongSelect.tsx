da637e53f3c685ca1e99acc59104f147a2992afc
Nava Choudhury <nava.choudhury@students.unsw.edu.au>
2025-11-09 18:51:12 +1100

    Basic Song Select Functionality

--- frontend/src/components/SongSelect.tsx
+++ frontend/src/components/SongSelect.tsx
@@ -1,8 +1,25 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect, useRef } from 'react';
+import CD from "../../public/CD.svg";
+import '../App.css'; 
+
 const SongSelect = () => {  
   return (
     <>
-      <main className="flex justify-center items-center w-screen h-screen text-[#FFFFFF]">
-        <span className="text-3xl">Song Select</span>
+      <main className="flex flex-col justify-center items-center w-screen h-screen text-[#FFFFFF]">
+
+        <span className="text-3xl">Select a song!</span>
+
+        <Link to="/game" className="cursor-pointer hover:underline">
+          Song 1 - Author 1
+        </Link>
+        
+        <Link to="/game" className="cursor-pointer hover:underline">
+          Song 2 - Author 2
+        </Link>
+
+        <img src={CD} alt="" />
+
       </main>
     </>
   )
