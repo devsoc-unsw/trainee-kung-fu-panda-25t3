cb5b52f9049d58dc9468f8efd30f3c5067858a09
Brian Wibawa Adi <brian.adi098@gmail.com>
2025-11-25 12:59:41 +1100

    comments

--- frontend/src/components/SongSelect.tsx
+++ frontend/src/components/SongSelect.tsx
@@ -10,12 +10,20 @@ const SongSelect = () => {
 
         <span className="text-3xl">Select a song!</span>
 
+        {/* somehow read all .osu files in the beatmapsRaw folder and display them in a list */}
+        {/* and somehow send the file name to the game */}
+        {/* parsing file data has been done in maploader.tsx */}
+        {/* probs need to split that and make a component that would work in song select and maploader */}
         <Link to="/game" className="cursor-pointer hover:underline">
-          Song 1 - Author 1
+          Song 1 - Author 1 - difficulty 1
         </Link>
         
         <Link to="/game" className="cursor-pointer hover:underline">
-          Song 2 - Author 2
+          Song 1 - Author 1 - difficulty 2
+        </Link>
+        
+        <Link to="/game" className="cursor-pointer hover:underline">
+          Song 2 - Author 2 - difficulty 1
         </Link>
 
         <img src={CD} alt="" />
