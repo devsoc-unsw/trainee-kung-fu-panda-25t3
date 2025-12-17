1031ea53b0889641be73fb8fe97036e796265300
June Delos Reyes <julia.delosreyes15@gmail.com>
2025-12-17 23:59:42 +1100

    inituserdata

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -6,6 +6,8 @@ import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 // import PlayerLoader from "./PlayerLoader";
 import "./Settings.css";
 import MenuButton from "./MenuButton";
+// import { type UserData } from "./CommonGame";
+// import InitUserData from "./InitUserData";
 
 type SettingsProps = {
   open: boolean;
@@ -13,6 +15,14 @@ type SettingsProps = {
 };
 
 const Settings = ({ open, onClose }: SettingsProps) => {
+  const keybindButtonFocus = () => {
+    console.log("is active");
+  };
+
+  const keybindButtonBlur = () => {
+    console.log("is blur");
+  };
+
   return (
     <Modal open={open} onClose={onClose}>
       <div className="flex flex-col justify-center items-center w-screen h-screen gap-3 text-[#FFFFFF]">
@@ -47,7 +57,12 @@ const Settings = ({ open, onClose }: SettingsProps) => {
               <div>
                 4k
                 <div className="flex justify-center place-items-center">
-                  <div className="keyBox" tabIndex={0}>
+                  <div
+                    className="keyBox"
+                    tabIndex={0}
+                    onFocus={keybindButtonFocus}
+                    onBlur={keybindButtonBlur}
+                  >
                     spc
                   </div>
                   <div className="keyBox" tabIndex={0}>
